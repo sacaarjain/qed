b2b117a72441af14669c8abd17ce713a9d7cfc72
brad <jeongjooho1995@gmail.com>
2023-04-22 18:17:57 -0700

    clean up for branch prep

--- src/Results.js
+++ src/Results.js
@@ -5,19 +5,28 @@ import {Image, Box} from 'grommet'
 export default function Results(props){
     if(!props.isCorrect)
         return (
-        <Box>
+        <Box align="center" justify="center" pad="small" background={{"color":"accent-4"}}>
+            
+        <CardHeader>
+            <Text pad="large">Oops, that didn't work!</Text>
+        </CardHeader>
+            
+            
             <Image
             fit="cover"
-            src="./nope-try-again.jpeg"
+            src="https://media.giphy.com/media/VL48WGMDjD64umCEkv/giphy.gif"
             />
         </Box>
         );
     
     return(
-    <Box>
+    <Box align="center" justify="center" pad="small" background={{"color":"graph-1"}}>
+        <CardHeader>
+                <Text pad="large">You nailed it!</Text>
+        </CardHeader>
         <Image
             fit="cover"
-            src="./qed.png"
+            src="https://media.tenor.com/5Fj7uqIZ22oAAAAC/highfive.gif"
         />
     </Box>
     );
